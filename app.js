9f11c954caa45420683ed3f85f1d5b3e8f573d0a
AnwarWajhi <anwarwajhi5@gmail.com>
2018-02-24 16:51:05 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+var i=0;
+
+function counter(){
+  i=i+1; //incrementing the value of i on every count beginning from 1
+  console.log("count="+i); //print i in console
+}
+
+
+counter();//calling counter() 1st time
+counter();//calling counter() 2nd time
+counter();//...
+counter();//...
+counter();//...
+counter();//...
